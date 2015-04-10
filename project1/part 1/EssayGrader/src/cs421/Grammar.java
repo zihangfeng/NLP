6d0a70816e4d2c49bddcbf96eed1e03855291011
zihangfeng <zihangfeng@gmail.com>
2015-04-10 12:13:14 -0500

    Grammar

--- project1/part 1/EssayGrader/src/cs421/Grammar.java
+++ project1/part 1/EssayGrader/src/cs421/Grammar.java
@@ -1,5 +1,41 @@
 package cs421;
 
-public class Grammar {
 
+import java.util.ArrayList;
+
+
+public class Grammar {
+	private double Spelling;
+	private double SubAgree;
+	private double VerbTense;
+	private double Format;
+	private int wordCount;
+	
+	public Grammar()
+	{
+		Spelling = 0.0;
+		SubAgree = 0.0;
+		VerbTense = 0.0;
+		Format = 0.0;
+		wordCount = 0;
+	}
+	
+	public double checkGarmmar(ArrayList<String> essay)
+	{
+		double grammarScore = 0.0;
+		countNumWord(essay);
+		
+		// the less it is the more points it gets
+		return (1 - grammarScore);
+	}
+	
+	private void countNumWord(ArrayList<String> essay)
+	{
+		int numSen = essay.size();
+		int i;
+		for(i = 0; i < numSen; i++)
+		{
+			wordCount += essay.get(i).length();
+		}
+	}
 }
