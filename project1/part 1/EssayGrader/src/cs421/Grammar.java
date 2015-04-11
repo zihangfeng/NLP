6bc651334cf2d0e698c292d88e76a882276ec38f
Evan <Evan@Evan-PC>
2015-04-11 17:35:42 -0500

    update

--- project1/part 1/EssayGrader/src/cs421/Grammar.java
+++ project1/part 1/EssayGrader/src/cs421/Grammar.java
@@ -15,32 +15,18 @@ import java.util.ArrayList;
 public class Grammar {
 	private static final Grammar Grammarinstance= new Grammar();
 	 
-	private double SpellingScore;
-	private double SubAgreeScore;
-	private double VerbTenseScore;
-	private double FormatScore;
-	private double GrammarScore;
-	private int wordCount;
+
 	
 	private Grammar()
 	{
-		SpellingScore = 0.0;
-		SubAgreeScore = 0.0;
-		VerbTenseScore = 0.0;
-		FormatScore = 0.0;
-		wordCount = 0;
-		GrammarScore = 0.0;
+
 	}
 	
 	public static Grammar getInstance() {
 		return Grammarinstance;
 	}
 	
-	public double getGrammarScore()
-	{
-		return GrammarScore;
-	}
-	
+
 	public double checkGarmmar(ArrayList<String> essay)
 	{
 		double grammarScore = 0.0;
@@ -56,16 +42,13 @@ public class Grammar {
 		int i;
 		for(i = 0; i < numSen; i++)
 		{
-			wordCount += essay.get(i).length();
+		//	wordCount += essay.get(i).length();
 		}
 	}
 	
 	public void Spelling(ArrayList<String> essay)
 	{
 		
-		SpellChecker spellchecker = new SpellChecker("textfiles");
-		
-		BasicDictionary dic = new 
 	}
 	
 	public void SubAgree(ArrayList<String> essay)
