7e3b9e72aa667d1b215255a359d88ad705885055
zihangfeng <zihangfeng@gmail.com>
2015-04-21 14:18:15 -0500

    update the Model class

---
+++ project1/part 1/EssayGrader/src/cs421/graderModer.java
@@ -0,0 +1,30 @@
+package cs421;
+
+import java.util.ArrayList;
+
+public class graderModer {
+	private ArrayList<Essay> essaySet;
+	private EssayResult[] model;
+	public graderModer(){ 
+		essaySet=new ArrayList<Essay>();
+		model=new EssayResult[3];
+	}
+    
+	public void readTrainingData(){
+		
+		
+	}
+    
+	public void analysisTrainingData(){
+		
+	}
+	
+	
+	public void updateAutoGraderModel(){
+		
+		
+	}
+	
+	public void updateModelFromfile(){}
+	
+}
